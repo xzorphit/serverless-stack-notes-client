5909f5b9b35006928aaa6c3a4738b86656c95bb0
xzorphit <52004595+xzorphit@users.noreply.github.com>
2020-10-21 12:59:46 -0400

    Login with AWS Cognito. Functional Login.

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Auth } from "aws-amplify";
 import "./Login.css";
 
 export default function Login() {
@@ -10,8 +11,16 @@ export default function Login() {
     return email.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+  
+    try {
+      await Auth.signIn(email, password);
+      alert("Logged in");
+    } catch (e) {
+    console.log(e);
+      alert(e.message);
+    }
   }
 
   return (
