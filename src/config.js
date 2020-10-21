c3c425184806421bdb0ad4efc1e04db1a0271be5
xzorphit <52004595+xzorphit@users.noreply.github.com>
2020-10-21 10:58:11 -0400

    Config AWS Amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "sstestbucket01"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://13hygvexp4.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_X9iw4EZsp",
+      APP_CLIENT_ID: "37d9f3j9srluqo4kdj7jj32dsi",
+      IDENTITY_POOL_ID: "us-east-1:71d3942a-7b8a-43db-a99c-730ca33b261c"
+    }
+  };
